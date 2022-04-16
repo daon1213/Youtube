5fd2584e69c0abe13a5a0417fe01a48335e61c95
daon1213 <diaeiyo1213@naver.com>
2022-04-16 19:36:43 +0900

    Video Model commit!

---
+++ app/src/main/java/com/daon/youtube_part4_01/model/VideoModel.kt
@@ -0,0 +1,9 @@
+package com.daon.youtube_part4_01.model
+
+data class VideoModel(
+    val title: String,
+    val sources: String,
+    val subtitle: String,
+    val thumb: String,
+    val description: String
+)
